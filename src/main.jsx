e88d1d4448a5777d5309b57517a9f1069f76b4e9
Jordan Bastian <100536830+weeef@users.noreply.github.com>
2026-05-31 20:39:16 -0500

    favicon update and package updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react";
 import Portfolio from './Portfolio.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Portfolio />
+    <Analytics />
   </React.StrictMode>,
 );
